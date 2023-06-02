1dc4936591241872c9f9dbfd7f71d8e905201a05
hardcommitoneself <hardcommitoneself@gmail.com>
2023-06-02 07:56:31 -0400

    feat: create tickets table and ticket model

---
+++ database/migrations/2023_06_02_102205_create_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('departure_time');
+            $table->string('source_airport');
+            $table->string('destination_airport');
+            $table->integer('seat')->unsigned();
+            $table->string('passport_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
